269548984833db579cd577807bf23bf243f87c3c
yemyintsoe <yms.yemyintsoe@gmail.com>
2022-11-20 12:12:34 +0630

    first commit by yms

---
+++ README.md
@@ -0,0 +1,28 @@
+# pinia-counter
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### this was added by yms to solve eslint error, eg. it doesn't accept defineProps if we don't add this line in package.json
+### "vue/setup-compiler-macros": true
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div>Parent Component</div>
+    <h2>{{ counterName }}</h2>
+    <h3>Counter - {{ count }}</h3>
+    <h3>Double Counter - {{ doubleCount }}</h3>
+    <button @click="counterStore.increment">+</button>
+    <button @click="counterStore.decrement">-</button>
+    <hr>
+    <ComponentB />
+    <hr>
+    <ComponentC />
+  </div>
+
+  <hr>
+</template>
+
+<script setup>
+  import { useCounterStore } from './stores/CounterStore'
+  import { storeToRefs } from 'pinia'
+  import ComponentB from './components/ComponentB.vue'
+  import ComponentC from './components/ComponentC.vue'
+  
+  const counterStore = useCounterStore()
+  const { count, counterName, doubleCount } = storeToRefs(counterStore)
+</script>
+
+<style>
+  .container { max-width: 300px; margin: 0 auto; }
+  button { display: inline-block; margin: 0 5px; padding: 0 10px; font-size: 25px; }
+</style>
